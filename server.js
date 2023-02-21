d2349d73930e3d0650626a8c02251d37be3f298f
Maitryee-Choudhary <67001353+Maitryee-Choudhary@users.noreply.github.com>
2023-02-21 20:38:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,14 +5,21 @@ const morgan = require('morgan');
 const transaction = require('./routes/transaction');
 const user = require('./routes/user');
 const connectDB = require('./config/db');
-
+const cors = require("cors");
 dotenv.config({path: './config/config.env'});
 const PORT = process.env.PORT || 5000;
 
 connectDB();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
+app.get('/',(req,res)=>{
+  res.header("Access-Control-Allow-Origin",'*');
+  res.header("Access-Control-Allow-Methods","GET,HEAD,OPTIONS,POST,PUT,DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Accept, Authorization");
+  res.send("Hi API is running");
+});
 app.use('/api/user', user)
 app.use('/api/transaction', transaction);
 
